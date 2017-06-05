1bf0010674b174a6f1a65f80282bfafb9c575441
Matthew Leibowitz <mattleibow@live.com>
2017-06-05 15:25:36 -0500

    Added SkiaSharp.Svg from the main repo

---
+++ SkiaSharp.Extended.Svg/samples/SkiaSharpDemo/SkiaSharpDemo/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace SkiaSharpDemo
+{
+	public partial class MainPage : TabbedPage
+	{
+		private SKSvg svg;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private static Stream GetImageStream(string svgName)
+		{
+			var type = typeof(MainPage).GetTypeInfo();
+			var assembly = type.Assembly;
+
+			return assembly.GetManifestResourceStream($"SkiaSharpDemo.images.{svgName}");
+		}
+
+		private void LoadSvg(string svgName)
+		{
+			// create a new SVG object
+			svg = new SKSvg();
+
+			// load the SVG document from a stream
+			using (var stream = GetImageStream(svgName))
+				svg.Load(stream);
+		}
+
+		private void OnPageAppearing(object sender, EventArgs e)
+		{
+			svg = null;
+
+			if (sender == logosPage)
+			{
+				LoadSvg("logos.svg");
+			}
+			else if (sender == transparencyPage)
+			{
+				LoadSvg("opacity.svg");
+			}
+
+			((SKCanvasView)((ContentPage)sender).Content).InvalidateSurface();
+		}
+
+		private void OnPainting(object sender, SKPaintSurfaceEventArgs e)
+		{
+			var surface = e.Surface;
+			var canvas = surface.Canvas;
+
+			var width = e.Info.Width;
+			var height = e.Info.Height;
+
+			// clear the surface
+			canvas.Clear(SKColors.White);
+
+			// the page is not visible yet
+			if (svg == null)
+				return;
+
+			// calculate the scaling need to fit to screen
+			float canvasMin = Math.Min(width, height);
+			float svgMax = Math.Max(svg.Picture.CullRect.Width, svg.Picture.CullRect.Height);
+			float scale = canvasMin / svgMax;
+			var matrix = SKMatrix.MakeScale(scale, scale);
+
+			// draw the svg
+			canvas.DrawPicture(svg.Picture, ref matrix);
+		}
+	}
+}
---
+++ SkiaSharp.Extended.Svg/tests/SkiaSharp.Extended.Svg.Tests/SKTest.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace SkiaSharp.Extended.Svg.Tests
+{
+	public abstract class SKTest
+	{
+		protected static readonly string PathToAssembly = Path.GetDirectoryName(typeof(SKTest).GetTypeInfo().Assembly.Location);
+		protected static readonly string PathToFonts = Path.Combine(PathToAssembly, "fonts");
+		protected static readonly string PathToImages = Path.Combine(PathToAssembly, "images");
+	}
+}
