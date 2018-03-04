87197fed0434989b8c0160e4e6ad8e7233a6aadb
Matthew Leibowitz <mattleibow@live.com>
2018-03-04 02:31:44 +0200

    Fix some tests

--- SkiaSharp.Extended.Svg/tests/SkiaSharp.Extended.Svg.Tests/SKTest.cs
+++ SkiaSharp.Extended.Svg/tests/SkiaSharp.Extended.Svg.Tests/SKTest.cs
@@ -5,8 +5,8 @@ namespace SkiaSharp.Extended.Svg.Tests
 {
 	public abstract class SKTest
 	{
-		protected static readonly string PathToAssembly = Path.GetDirectoryName(typeof(SKTest).GetTypeInfo().Assembly.Location);
-		protected static readonly string PathToFonts = Path.Combine(PathToAssembly, "fonts");
-		protected static readonly string PathToImages = Path.Combine(PathToAssembly, "images");
+		//protected static readonly string PathToAssembly = Path.GetDirectoryName(typeof(SKTest).GetTypeInfo().Assembly.Location);
+		protected static readonly string PathToFonts = Path.Combine("fonts");
+		protected static readonly string PathToImages = Path.Combine("images");
 	}
 }
