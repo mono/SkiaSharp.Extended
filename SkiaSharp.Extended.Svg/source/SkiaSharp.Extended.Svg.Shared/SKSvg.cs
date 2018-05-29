c656a3b9813b476b0a3eeeae927df8fa117bf3ea
Daniel <daniel.luberda@gmail.com>
2018-05-29 12:20:38 +0200

    SVG: Added stroke-linecap, stroke-linejoin, stroke-miterlimit support

--- SkiaSharp.Extended.Svg/source/SkiaSharp.Extended.Svg.Shared/SKSvg.cs
+++ SkiaSharp.Extended.Svg/source/SkiaSharp.Extended.Svg.Shared/SKSvg.cs
@@ -890,6 +890,15 @@ namespace SkiaSharp.Extended.Svg
                 var strokeMiterLimit = GetString(style, "stroke-miterlimit");
                 var hasStrokeMiterLimit = !string.IsNullOrWhiteSpace(strokeMiterLimit);
 
+				if (strokePaint == null)
+                {
+                    if (hasStrokeDashArray || hasStrokeWidth || hasStrokeOpacity
+                        || hasStrokeLineCap || hasStrokeLineJoin)
+                    {
+                        strokePaint = CreatePaint(true);
+                    }
+                }
+
 				if (hasStrokeDashArray)
 				{
 					if ("none".Equals(strokeDashArray, StringComparison.OrdinalIgnoreCase))
