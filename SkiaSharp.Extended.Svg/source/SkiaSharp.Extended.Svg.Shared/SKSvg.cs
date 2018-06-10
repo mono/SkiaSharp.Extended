04a620d879749f471c55e9cf70679a47730c11cb
Daniel <daniel.luberda@gmail.com>
2018-06-10 21:59:37 +0200

    fillPaint -> strokePaint (typo)

--- SkiaSharp.Extended.Svg/source/SkiaSharp.Extended.Svg.Shared/SKSvg.cs
+++ SkiaSharp.Extended.Svg/source/SkiaSharp.Extended.Svg.Shared/SKSvg.cs
@@ -886,7 +886,7 @@ namespace SkiaSharp.Extended.Svg
 					if (ColorHelper.TryParse(stroke, out SKColor color))
 					{
 						// preserve alpha
-						if (color.Alpha == 255 && fillPaint.Color.Alpha > 0)
+						if (color.Alpha == 255 && strokePaint.Color.Alpha > 0)
 							strokePaint.Color = color.WithAlpha(strokePaint.Color.Alpha);
 						else
 							strokePaint.Color = color;
