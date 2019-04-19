24e1a5197c4e01bedb243ee6b19a20b64d4f9afd
Daniel <daniel.luberda@gmail.com>
2019-04-19 11:16:55 +0200

    Create SKSvgMask.cs

---
+++ SkiaSharp.Extended.Svg/source/SkiaSharp.Extended.Svg.Shared/SKSvgMask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace SkiaSharp.Extended.Svg
+{
+	internal class SKSvgMask
+	{
+		public SKSvgMask(SKPaint fill, XElement element)
+		{
+			Fill = fill.Clone();
+			Element = element;
+		}
+
+		public SKPaint Fill { get; }
+
+		public XElement Element { get; }
+	}
+}
