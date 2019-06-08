56cce2d0409e829d8657c77fcaa0dc5658fe3ebc
Matthew Leibowitz <mattleibow@live.com>
2019-06-08 23:46:49 +0200

    Add initial controls

--- SkiaSharp.Extended.Controls/samples/SkiaSharpDemo/MainPage.xaml.cs
+++ SkiaSharp.Extended.Controls/samples/SkiaSharpDemo/MainPage.xaml.cs
@@ -1,14 +1,54 @@
 ﻿using System;
 using SkiaSharp;
+using SkiaSharp.Extended.Controls;
 using Xamarin.Forms;
 
 namespace SkiaSharpDemo
 {
 	public partial class MainPage : ContentPage
 	{
+		private bool useHardware;
+
 		public MainPage()
 		{
 			InitializeComponent();
+
+			BindingContext = this;
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			// set up a nice render loop
+			Device.StartTimer(TimeSpan.FromSeconds(1.0 / 60.0), () =>
+			{
+				dynamicSurface?.InvalidateSurface();
+				return true;
+			});
+		}
+
+		public bool UseHardware
+		{
+			get => useHardware;
+			set
+			{
+				useHardware = value;
+				OnPropertyChanged();
+			}
+		}
+
+		SKPaint paint = new SKPaint { TextSize = 40 };
+
+		private void OnPainting(object sender, SKPaintDynamicSurfaceEventArgs e)
+		{
+			var canvas = e.Surface.Canvas;
+
+			canvas.Clear(SKColors.Transparent);
+
+			canvas.DrawText($"ticks: {Environment.TickCount}", 200, 200, paint);
+			canvas.DrawText($"time:  {DateTime.Now}", 200, 250, paint);
+			canvas.DrawText($"is hw: {e.IsHardwareAccelerated}", 200, 300, paint);
 		}
 	}
 }
---
+++ SkiaSharp.Extended.Controls/source/SKDynamicSurfaceView.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace SkiaSharp.Extended.Controls
+{
+	public class SKDynamicSurfaceView : Layout<View>
+	{
+		public static readonly BindableProperty IsHardwareAcceleratedProperty = BindableProperty.Create(
+			nameof(IsHardwareAccelerated),
+			typeof(bool),
+			typeof(SKDynamicSurfaceView),
+			false,
+			propertyChanged: OnIsHardwareAcceleratedChanged);
+
+		private SKCanvasView canvasView;
+		private SKGLView glView;
+
+		public SKDynamicSurfaceView()
+		{
+			SwapSurface(false);
+		}
+
+		public bool IsHardwareAccelerated
+		{
+			get => (bool)GetValue(IsHardwareAcceleratedProperty);
+			set => SetValue(IsHardwareAcceleratedProperty, value);
+		}
+
+		public event EventHandler<SKPaintDynamicSurfaceEventArgs> PaintSurface;
+
+		public void InvalidateSurface()
+		{
+			if (canvasView?.IsVisible == true)
+				canvasView?.InvalidateSurface();
+
+			if (glView?.IsVisible == true)
+				glView?.InvalidateSurface();
+		}
+
+		protected override void LayoutChildren(double x, double y, double width, double height)
+		{
+			foreach (var child in Children)
+			{
+				if (!child.IsVisible)
+					continue;
+
+				if (child == canvasView || child == glView)
+					child.Layout(new Rectangle(0, 0, Width, Height));
+				else
+					child.Layout(new Rectangle(x, y, width, height));
+			}
+		}
+
+		protected virtual void OnPaintSurface(SKPaintDynamicSurfaceEventArgs e) =>
+			PaintSurface?.Invoke(this, e);
+
+		private void SwapSurface(bool hardware)
+		{
+			if (hardware)
+			{
+				glView = new SKGLView();
+				glView.PaintSurface += OnPaintSurfaceHandler;
+				Children.Insert(0, glView);
+
+				if (canvasView != null)
+				{
+					canvasView.PaintSurface -= OnPaintSurfaceHandler;
+					canvasView.IsVisible = false;
+					Children.Remove(canvasView);
+					canvasView = null;
+				}
+			}
+			else
+			{
+				canvasView = new SKCanvasView();
+				canvasView.PaintSurface += OnPaintSurfaceHandler;
+				Children.Insert(0, canvasView);
+
+				if (glView != null)
+				{
+					glView.PaintSurface -= OnPaintSurfaceHandler;
+					glView.IsVisible = false;
+					Children.Remove(glView);
+					glView = null;
+				}
+			}
+		}
+
+		private void OnPaintSurfaceHandler(object sender, SKPaintSurfaceEventArgs e) =>
+			OnPaintSurface(new SKPaintDynamicSurfaceEventArgs(e));
+
+		private void OnPaintSurfaceHandler(object sender, SKPaintGLSurfaceEventArgs e) =>
+			OnPaintSurface(new SKPaintDynamicSurfaceEventArgs(e));
+
+		private static void OnIsHardwareAcceleratedChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			if (bindable is SKDynamicSurfaceView view && newValue is bool hardware)
+				view.SwapSurface(hardware);
+		}
+	}
+}
