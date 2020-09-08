d5984a4b9c87537ced192cc38edd8ad1a6eca58a
Matthew Leibowitz <mattleibow@live.com>
2020-09-08 23:17:49 +0200

    Added some WPF tests

---
+++ tests/SkiaSharp.Extended.UI.WPF.Tests/TestUtils.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace SkiaSharp.Extended.UI.WPF.Tests
+{
+	public static class TestUtils
+	{
+		static TestUtils()
+		{
+			var app = new System.Windows.Application();
+
+			Forms.Init();
+		}
+
+		public static void EnsureFormsInit()
+		{
+		}
+	}
+}
