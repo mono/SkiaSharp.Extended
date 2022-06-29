21b474d0f9b038336dc544ce8b30e6a1a21c6286
Matthew Leibowitz <mattleibow@live.com>
2022-06-29 02:19:59 +0200

    Update SKUriLottieImageSourceTest.cs

--- tests/SkiaSharp.Extended.UI.Tests/Controls/Lottie/SKUriLottieImageSourceTest.cs
+++ tests/SkiaSharp.Extended.UI.Tests/Controls/Lottie/SKUriLottieImageSourceTest.cs
@@ -2,8 +2,8 @@
 
 public class SKUriLottieImageSourceTest : SKLottieImageSourceTest<SKUriLottieImageSource>
 {
-	private const string TrophyJson = "https://raw.githubusercontent.com/mono/SkiaSharp.Extended/dev/lottie/tests/SkiaSharp.Extended.UI.Tests/TestAssets/Lottie/trophy.json";
-	private const string LoloJson = "https://raw.githubusercontent.com/mono/SkiaSharp.Extended/dev/lottie/tests/SkiaSharp.Extended.UI.Tests/TestAssets/Lottie/lolo.json";
+	private const string TrophyJson = "https://raw.githubusercontent.com/mono/SkiaSharp.Extended/main/tests/SkiaSharp.Extended.UI.Tests/TestAssets/Lottie/trophy.json";
+	private const string LoloJson = "https://raw.githubusercontent.com/mono/SkiaSharp.Extended/main/lottie/tests/SkiaSharp.Extended.UI.Tests/TestAssets/Lottie/lolo.json";
 
 	protected override SKUriLottieImageSource CreateEmptyImageSource() =>
 		new SKUriLottieImageSource { };
