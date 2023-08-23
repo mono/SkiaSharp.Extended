58779a07a8184e75247806a37a9c48a1f3f2cc14
Matthew Leibowitz <mattleibow@live.com>
2023-08-23 02:00:35 +0200

    Allow for a slightly delayed theme registration (#200)

--- source/SkiaSharp.Extended.UI/Controls/Confetti/SKConfettiViewResources.shared.xaml.cs
+++ source/SkiaSharp.Extended.UI/Controls/Confetti/SKConfettiViewResources.shared.xaml.cs
@@ -2,13 +2,8 @@
 
 public partial class SKConfettiViewResources : ResourceDictionary
 {
-	private static bool registered;
-
 	public SKConfettiViewResources()
 	{
 		InitializeComponent();
 	}
-
-	internal static void EnsureRegistered() =>
-		ResourceLoader.EnsureRegistered<SKConfettiViewResources>(ref registered);
 }
--- source/SkiaSharp.Extended.UI/Controls/Lottie/SKLottieViewResources.shared.xaml.cs
+++ source/SkiaSharp.Extended.UI/Controls/Lottie/SKLottieViewResources.shared.xaml.cs
@@ -2,13 +2,8 @@
 
 public partial class SKLottieViewResources : ResourceDictionary
 {
-	private static bool registered;
-
 	public SKLottieViewResources()
 	{
 		InitializeComponent();
 	}
-
-	internal static void EnsureRegistered() =>
-		ResourceLoader.EnsureRegistered<SKLottieViewResources>(ref registered);
 }
