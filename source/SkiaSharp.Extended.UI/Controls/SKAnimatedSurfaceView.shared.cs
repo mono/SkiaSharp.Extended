d1a55e833f261372e5224d3c90837cbf93044be5
Matthew Leibowitz <mattleibow@live.com>
2024-04-08 12:49:26 +0200

    this

--- source/SkiaSharp.Extended.UI/Controls/SKAnimatedSurfaceView.shared.cs
+++ source/SkiaSharp.Extended.UI/Controls/SKAnimatedSurfaceView.shared.cs
@@ -82,8 +82,10 @@ public class SKAnimatedSurfaceView : SKSurfaceView
 				if (!weakThis.TryGetTarget(out var strongThis))
 					return false;
 
+#if !XAMARIN_FORMS
 				if (strongThis.Window is null)
 					return false;
+#endif
 
 				if (!strongThis.IsLoadedEx())
 					return false;
