0602e7756e99e1f86084c77c85d19935f4ffa98f
Matthew Leibowitz <mattleibow@live.com>
2025-09-04 00:53:10 +0200

    oops, what am i doing???

--- source/SkiaSharp.Extended.UI.Maui/Controls/SKSurfaceView.shared.cs
+++ source/SkiaSharp.Extended.UI.Maui/Controls/SKSurfaceView.shared.cs
@@ -19,7 +19,7 @@ public class SKSurfaceView : TemplatedView
 
 	public SKSurfaceView()
 	{
-		ResourceLoader<Themes.SKSurfaceViewResources>.EnsureRegistered(this);
+		ResourceLoader<Themes.SKSurf aceViewResources>.EnsureRegistered(this);
 
 		DebugUtils.LogPropertyChanged(this);
 	}
